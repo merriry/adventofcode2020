b3f9efccd0de6e090f211f080cc4d22702f65617
merriry <ryan@merrill-johnson.com>
2020-12-17 09:31:38 -0700

    day 12 solutions

---
+++ 12-1.ps1
@@ -0,0 +1,20 @@
+﻿$ErrorActionPreference = "silentlycontinue"
+Remove-Variable *
+
+
+$position = @(0,0)
+$facing = 1
+$facings = @(@(-1,0), @(0,1), @(1,0), @(0,-1))
+Get-Content .\day12input | ForEach-Object {
+    if ($_ -match "^(\w)(\d+)") {
+    switch ($matches.1) {
+    "N" {$position[0] -= $matches.2}
+    "E" {$position[1] += $matches.2}
+    "S" {$position[0] += $matches.2}
+    "W" {$position[1] -= $matches.2}
+    "R" {$facing = ($facing + $($matches.2)/90) % 4}
+    "L" {$facing = (($facing - $($matches.2)/90) % 4 + 4) % 4}
+    "F" {$position[0] += $facings[$facing][0] * $matches.2; $position[1] += $facings[$facing][1] * $matches.2 }
+    }}
+    }
+    [math]::Abs($position[0]) + [math]::Abs($position[1])
---
+++ 12-2.ps1
@@ -0,0 +1,19 @@
+﻿$ErrorActionPreference = "silentlycontinue"
+Remove-Variable *
+
+
+$position = @(0,0)
+$waypoint = @(10,1)
+Get-Content .\day12input | ForEach-Object {
+    if ($_ -match "^(\w)(\d+)") {
+    switch ($matches.1) {
+    "N" {$waypoint[1] += $matches.2}
+    "E" {$waypoint[0] += $matches.2}
+    "S" {$waypoint[1] -= $matches.2}
+    "W" {$waypoint[0] -= $matches.2}
+    "R" {1..($($matches.2)/90) | ForEach-Object {$waypoint = @($waypoint[1],-$waypoint[0])}}
+    "L" {1..($($matches.2)/90) | ForEach-Object {$waypoint = @(-$waypoint[1],$waypoint[0])}}
+    "F" {$position[0] += $waypoint[0] * $matches.2; $position[1] += $waypoint[1] * $matches.2 }
+    }}
+    }
+    [math]::Abs($position[0]) + [math]::Abs($position[1])
