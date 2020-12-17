ce2593f2b9ac1fd89e7a2f2714561869b3373c2f
merriry <ryan@merrill-johnson.com>
2020-12-17 11:24:21 -0700

    day 13 solutions

---
+++ day13-1.ps1
@@ -0,0 +1,7 @@
+﻿$ErrorActionPreference = "silentlycontinue"
+Remove-Variable *
+$file = Get-Content .\day13input
+$minstart = $time = [int]$file[0]
+[System.Collections.ArrayList]$bus = $file[1].Split(",") |where {$_ -ne "x"}
+$bus | ForEach-Object { if ($_ - ($minstart % $_) -lt $time) {$time = $_ - ($minstart % $_); $busno = $_ }}
+$time * $busno
---
+++ day13-2.ps1
@@ -0,0 +1,17 @@
+﻿$ErrorActionPreference = "silentlycontinue"
+Remove-Variable *
+$file = Get-Content .\day13input
+[System.Collections.ArrayList]$bus = $file[1].Split(",")
+[int64]$time = 0
+[int64]$mult = 1
+$bus | ForEach-Object {
+    
+    if ($_ -eq "x") {return}
+    $remain = ($_ - $bus.IndexOf([string]$_)) % $_
+    if ($remain -lt 0) {$remain += $_}
+    while ($time % $_ - $remain -ne 0) {
+        $time += $mult
+    }
+    $mult *= $_
+    }
+    $time
