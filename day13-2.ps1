7483ad18664ef9499f0b9ee0c1893c3983e9ba80
merriry <ryan@merrill-johnson.com>
2020-12-17 11:28:17 -0700

    Update day13-2.ps1

--- day13-2.ps1
+++ day13-2.ps1
@@ -1,17 +1,12 @@
 ﻿$ErrorActionPreference = "silentlycontinue"
 Remove-Variable *
-$file = Get-Content .\day13input
-[System.Collections.ArrayList]$bus = $file[1].Split(",")
+[System.Collections.ArrayList]$bus = (Get-Content .\day13input)[1].Split(",")
 [int64]$time = 0
 [int64]$mult = 1
 $bus | ForEach-Object {
-    
     if ($_ -eq "x") {return}
-    $remain = ($_ - $bus.IndexOf([string]$_)) % $_
-    if ($remain -lt 0) {$remain += $_}
-    while ($time % $_ - $remain -ne 0) {
-        $time += $mult
-    }
+    $remain = ($_ - $bus.IndexOf([string]$_)) % $_ + $(if ($_ - $bus.IndexOf([string]$_) -lt 0) {$_})
+    while ($time % $_ - $remain -ne 0) { $time += $mult }
     $mult *= $_
     }
     $time
