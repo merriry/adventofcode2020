f6cd4d7f44870406dd995f6fee37c83ac3e33e38
merriry <ryan@merrill-johnson.com>
2020-12-22 14:38:35 -0700

    day 15 solutions

---
+++ day15-1.ps1
@@ -0,0 +1,17 @@
+﻿$ErrorActionPreference = "silentlycontinue"
+Remove-Variable *
+$totalrounds = 2020
+$nums = @(-1) * $totalrounds
+$round = 0
+$last = 0
+[int[]](Get-Content .\day15input).Split(",") | foreach { $nums[$_] = $round; $last = $_; $round +=1}
+$nums[$last] = -1
+$round -= 1
+do {
+    if ($nums[$last] -eq -1) { $newlast = 0}
+    else { $newlast = $round - $nums[$last]}
+    $nums[$last] = $round
+    $last = $newlast
+    $round += 1
+} while ($round -lt $totalrounds)
+[array]::IndexOf($nums, $round-1)
